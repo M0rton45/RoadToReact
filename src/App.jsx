a598c299389e506a1a717fce1a7d8acdb99bd742
Mateusz Kurdyk <matic@MacBook-Air-Mateusz.local>
2026-07-29 11:28:03 +0200

    object pizzas add

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,29 @@
 // import { useState } from 'react'
-// import reactLogo from './assets/react.svg'
-// import viteLogo from './assets/vite.svg'
-// import heroImg from './assets/hero.png'
 import './App.css'
+
+const pizzas = [
+  {
+    type: 'margherita',
+    url: 'https://aniagotuje.pl/przepis/pizza-margherita',
+    img: './assets/margherita.jpg',
+    price: 24.90,
+    id: 0,
+  },
+  {
+    type: 'pepperoni',
+    url: 'https://aniagotuje.pl/przepis/pizza-pepperoni',
+    img: './assets/pepperoni.jpg',
+    price: 29.90,
+    id: 1,
+  }
+]
 
 function App() {
   // const [count, setCount] = useState(0)
 
+  // const title = 'React';
   //  <> and </> it's just a empty div
+  console.log(pizzas[0])
   return (
     <>
       <label htmlFor="search">Search: </label>
